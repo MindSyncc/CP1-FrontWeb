394776895125ac3be2c01ef3f2263420460d794d
Fernando C <thelonder461@gmail.com>
2024-09-03 19:19:49 -0300

    Iniciando a estilização do Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,16 +1,18 @@
-import { NavStyle } from '../css/FooterStyle'
+import { NavStyle } from '../css/NavStyle'
 
-const Nav =()=>{
+const Nav =() => {
     return(
         
         <NavStyle>
-        <header>
-            <ul>
-            <li>Home</li>
-            <li>Produtos</li>
-            <li>Sobre</li>
-            <li>Contato</li>
-            </ul>
+        <header className='headerwrapper'>
+            <nav className='nav-list'>
+                <ul>
+                    <li>Home</li>
+                    <li>Produtos</li>
+                    <li>Sobre</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
         </header>
        </NavStyle>
     )
--- src/css/NavStyle.jsx
+++ src/css/NavStyle.jsx
@@ -2,10 +2,23 @@ import styled from 'styled-components'
 
 export const NavStyle =styled.section `
 
-.background{
-    background-color: blue;
+.nav-list{
+    background-color: grey;
     width: 100%;
-    height: 20vh;
+    height: 15vh;
+}
+
+.nav-list ul{
+    display: flex;
+    text-decoration: none;
+    list-style: none;
+    gap: 20px;
+}
+
+.nav-list li{
+    cursor: pointer;
+
+
 }
 
 `
