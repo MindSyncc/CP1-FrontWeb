6a734f91f57d65a1d6f00d60d75acdfaa517fbd6
Julia Carolina Ferreira Silva <juliacarolina07@gmail.com>
2024-09-07 17:33:47 -0300

    Adicionando responsividade ao menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,32 +1,39 @@
 // src/components/Nav.jsx
-
-
-import { Link } from 'react-router-dom'; // Importe o Link do react-router-dom
-
-import '../css/Nav.css';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import '../css/Nav.css'; // Verifique se o caminho está correto
 
 const Nav = () => {
-    return (
-        <nav className='container'>
-            <h1 className='logo'>Power Sports</h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/sobre">Sobre</Link>
-                </li>
-                <li>
-                    <Link to="/contato">Contato</Link>
-                </li>
-                <li>
-                    <Link to="/produtosdetails">
-                        <button className='btn'>Nossos Produtos</button>
-                    </Link>
-                </li>
-            </ul>
-        </nav>
-    );
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <nav className='container'>
+      <h1 className='logo'>Power Sports</h1>
+      <button className='menu-toggle' onClick={toggleMenu}>
+        ☰
+      </button>
+      <ul className={isMenuOpen ? 'show' : ''}>
+        <li className='link-home'>
+          <Link to="/">Home</Link>
+        </li>
+        <li className='link-home'>
+          <Link to="/sobre">Sobre</Link>
+        </li>
+        <li className='link-home'>
+          <Link to="/contato">Contato</Link>
+        </li>
+        <li id='botao-home'>
+          <Link to="/produtosdetails">
+            <button className='botao'>Nossos Produtos</button>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
